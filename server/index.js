6ef372d37b222054d0e7afb1c1316d1524c63e38
suryakantarout464-colla <suryakantarout464@gmail.com>
2026-05-13 18:09:58 +0530

    final fix routing

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
